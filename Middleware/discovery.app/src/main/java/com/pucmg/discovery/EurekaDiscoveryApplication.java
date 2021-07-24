c5d2e2f13aef58643b612f7cad1f4d6fe378ea38
Guilherme Batista <guilherme.da.franca.batista@gmail.com>
2021-07-24 13:40:15 -0300

    updating gateway and discovery components

--- Middleware/discovery.app/src/main/java/com/pucmg/discovery/EurekaDiscoveryApplication.java
+++ Middleware/discovery.app/src/main/java/com/pucmg/discovery/EurekaDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package br.com.sigo.eurekadiscovery;
+package com.pucmg.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
